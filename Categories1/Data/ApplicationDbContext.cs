f27a7541705c0e756edd8711355a3c65afeb3eb1
Мартин Димитров <md18191236@edu.mon.bg>
2024-10-11 12:06:30 +0300

    Add project files.

---
+++ Categories1/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Categories1.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Categories1.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
